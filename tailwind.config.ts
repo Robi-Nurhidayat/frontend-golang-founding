ef9a06488ba796b95c273d86bb5db72318a63e06
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-10-25 08:28:24 +0700

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        outline: "0 0 0 1px rgba(66, 153, 225, 0.5)",
+      },
+      colors: {
+        "orange-button": "#FF872E",
+        "green-button": "#1ABC9C",
+        "purple-hover": "#4C52F8",
+        "purple-hover-stroke": "#8286FF",
+        "purple-progress": "#3B41E3",
+      },
+      borderRadius: {
+        20: "20px",
       },
     },
   },
+  // fontFamily: {
+  //   sans: ["Poppins", "sans-serif"],
+  // },
   plugins: [],
-}
-export default config
+};
+export default config;
