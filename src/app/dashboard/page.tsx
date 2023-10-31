33775ec952c10aa513d364a0f770234dcdeee57d
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-10-31 08:14:30 +0700

    finishing dashboard your projects section

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -26,22 +26,68 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className="px-[100px]">
-        <div>
-          <div>
-            <Image src={Test} alt="image" />
+      <div className="px-[100px] mb-10 flex flex-col gap-y-5">
+        {/* project 1 */}
+        <div className="h-[250px] flex gap-x-8 border items-center">
+          <div className="h-full">
+            <Image
+              src={Test}
+              alt="image"
+              className="h-full w-[80%] object-cover"
+            />
           </div>
-          <div>
-            <h2>Cari uang buat Gunpla</h2>
-            <h5>
+          <div className=" py-4 pr-8">
+            <h2 className="text-[22px] font-bold text-[#222] mb-2">
+              Cari uang buat Gunpla
+            </h2>
+            <h5 className="mb-2 text-[18px] text-gray-500">
               Rp. 2000.000,000 ' <span>80%</span>
             </h5>
-            <p>
+            <p className="mb-8 text-[#222] font-[300]">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
               harum ratione minus hic, qui consequatur deleniti, nesciunt vitae
               nulla tempore magni ipsa. Unde dolor veniam perspiciatis, cum
               dolores nesciunt quia.
             </p>
+
+            <Link
+              href={"/"}
+              className="text-white font-semibold rounded py-3 px-4 bg-green-500"
+            >
+              Detail
+            </Link>
+          </div>
+        </div>
+
+        {/* projct 2 */}
+        <div className="h-[250px] flex gap-x-8 border items-center">
+          <div className="h-full">
+            <Image
+              src={Test}
+              alt="image"
+              className="h-full w-[80%] object-cover"
+            />
+          </div>
+          <div className=" py-4 pr-8">
+            <h2 className="text-[22px] font-bold text-[#222] mb-2">
+              Cari uang buat Gunpla
+            </h2>
+            <h5 className="mb-2 text-[18px] text-gray-500">
+              Rp. 2000.000,000 ' <span>80%</span>
+            </h5>
+            <p className="mb-8 text-[#222] font-[300]">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
+              harum ratione minus hic, qui consequatur deleniti, nesciunt vitae
+              nulla tempore magni ipsa. Unde dolor veniam perspiciatis, cum
+              dolores nesciunt quia.
+            </p>
+
+            <Link
+              href={"/"}
+              className="text-white font-semibold rounded py-3 px-4 bg-green-500"
+            >
+              Detail
+            </Link>
           </div>
         </div>
       </div>
